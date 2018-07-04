f1c87cd9d5449f7d1f976ef6c6e03eb948ee17f4
Daisuke Shibata <skm.spc0620@gmail.com>
2018-07-04 19:12:01 +0900

    Add activity for image search

---
+++ app/src/main/java/com/example/ntt_test/nlutestapp/ImageActivity.java
@@ -0,0 +1,44 @@
+package com.example.ntt_test.nlutestapp;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class ImageActivity extends AppCompatActivity {
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    static String subscriptionKey = "678a4900e849413380f04c00066662a5";
+
+    // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+    // search APIs.  In the future, regional endpoints may be available.  If you
+    // encounter unexpected authorization errors, double-check this value against
+    // the endpoint for your Bing Web search instance in your Azure dashboard.
+    static String host = "https://api.cognitive.microsoft.com";
+    static String path = "/bing/v7.0/images/search";
+
+    static String searchTerm = "puppies";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        TestTask test = new TestTask();
+        test.execute();
+    }
+}
--- app/src/main/java/com/example/ntt_test/nlutestapp/TestTask.java
+++ app/src/main/java/com/example/ntt_test/nlutestapp/TestTask.java
@@ -1,33 +1,46 @@
 package com.example.ntt_test.nlutestapp;
 
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class TestTask extends AsyncTask<Integer, Integer, Integer> {
+
+    // Replace the subscriptionKey string value with your valid subscription key.
+    static String subscriptionKey = "678a4900e849413380f04c00066662a5";
+
+    // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
+    // search APIs.  In the future, regional endpoints may be available.  If you
+    // encounter unexpected authorization errors, double-check this value against
+    // the endpoint for your Bing Web search instance in your Azure dashboard.
+    static String host = "https://api.cognitive.microsoft.com";
+    static String path = "/bing/v7.0/images/search";
+
+    static String searchTerm = "星空凛";
+
 
     private Listener listener;
 
     // 非同期処理
     @Override
     protected Integer doInBackground(Integer... params) {
-
-        // 10秒数える処理
-        do{
-            try {
-                //　1sec sleep
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-
-            Log.d("debug",""+params[0]);
-            params[0]++;
-            // 途中経過を返す
-            publishProgress(params[0]);
-
-        }while(params[0]<10);
-
-        return params[0] ;
+        imageGet();
+        return null;
     }
 
     // 途中経過をメインスレッドに返す
@@ -53,5 +66,76 @@ public class TestTask extends AsyncTask<Integer, Integer, Integer> {
 
     interface Listener {
         void onSuccess(int count);
+    }
+
+    public static SearchResults SearchImages(String searchQuery) throws Exception {
+        // construct URL of search request (endpoint + query string)
+        URL url = new URL(host + path + "?q=" + URLEncoder.encode(searchQuery, "UTF-8"));
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+        // receive JSON body
+        InputStream stream = connection.getInputStream();
+        String response = new Scanner(stream).useDelimiter("\\A").next();
+
+        // construct result object for return
+        SearchResults results = new SearchResults(new HashMap<String, String>(), response);
+
+        // extract Bing-related HTTP headers
+        Map<String, List<String>> headers = connection.getHeaderFields();
+        for (String header : headers.keySet()) {
+            if (header == null) continue;      // may have null key
+            if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
+                results.relevantHeaders.put(header, headers.get(header).get(0));
+            }
+        }
+
+        stream.close();
+        return results;
+    }
+
+    // pretty-printer for JSON; uses GSON parser to parse and re-serialize
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+
+    public static void imageGet() {
+        if (subscriptionKey.length() != 32) {
+            System.out.println("Invalid Bing Search API subscription key!");
+            System.out.println("Please paste yours into the source code.");
+            System.exit(1);
+        }
+
+
+        try {
+            System.out.println("Searching the Web for: " + searchTerm);
+
+            SearchResults result = SearchImages(searchTerm);
+
+            System.out.println("\nRelevant HTTP Headers:\n");
+            for (String header : result.relevantHeaders.keySet())
+                System.out.println(header + ": " + result.relevantHeaders.get(header));
+
+            System.out.println("\nJSON Response:\n");
+            System.out.println(prettify(result.jsonResponse));
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
+    }
+
+}
+
+// Container class for search results encapsulates relevant headers and JSON data
+class SearchResults{
+    HashMap<String, String> relevantHeaders;
+    String jsonResponse;
+    SearchResults(HashMap<String, String> headers, String json) {
+        relevantHeaders = headers;
+        jsonResponse = json;
     }
 }
